de50f2310c48ade4dece3c9855e6ca1882556a25
schakravarti <schakravarti930@gmail.com>
2020-10-04 10:26:12 +0530

    [Swapnil] Add : Added Contact Class

--- AddressBookSystem/AddressBookMain.cs
+++ AddressBookSystem/AddressBookMain.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
     class AddressBookMain
     {
+        public List<Contact> ContactList;
+        public AddressBookMain()
+        {
+            this.ContactList = new List<Contact>();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Program");
