11d34ae9fefbb0469bb4e1c57f74860c02212bd9
schakravarti <schakravarti930@gmail.com>
2020-10-04 09:44:28 +0530

    [Swapnil] Add : Welcome Message

---
+++ AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program");
+        }
+    }
+}
